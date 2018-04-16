5e1538382dc1f6a4ba4c6875075dea503b6dc9b7
DANIEL BEZERRA <dbezerra@wkm.com.br>
2018-04-16 13:39:28 -0300

    criando home view e table componente, instalando os pacotes que vao ser usados pra organizar a tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatTableModule,MatSortModule} from '@angular/material';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { TableComponent } from './table/table.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent
-  ],
+,
+    HomeComponent,
+    TableComponent
+],
   imports: [
     BrowserModule
   ],
